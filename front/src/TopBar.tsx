736eb3b6e564fd993e018d8c7daeea2f05336f95
Igor Silva <asilvaigor@gmail.com>
2022-10-02 02:10:15 +0200

    Separate TopBar and Footer

---
+++ front/src/TopBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import CameraIcon from '@mui/icons-material/PhotoCamera'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+
+export default function TopBar() {
+  return (
+    <AppBar position="relative">
+      <Toolbar>
+        <CameraIcon sx={{ mr: 2 }} />
+        <Typography variant="h6" color="inherit" noWrap>
+          Album layout
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
