05b3356b48f6788373b77399b4b7aa602d3f7d09
Igor Silva <asilvaigor@gmail.com>
2022-10-02 02:10:15 +0200

    Separate TopBar and Footer

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import Album from './Album'
+import TopBar from './TopBar'
 import theme from './theme'
+import Footer from './Footer'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement!)
@@ -12,6 +14,8 @@ root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
+    <TopBar />
     <Album />
+    <Footer />
   </ThemeProvider>
 )
