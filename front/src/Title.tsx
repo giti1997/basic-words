ad36d21cb5af9e81cfc6476b4bd295a8211d9038
Igor Silva <asilvaigor@gmail.com>
2022-10-16 23:54:49 +0200

    Add CustomAutocomplete

--- front/src/Title.tsx
+++ front/src/Title.tsx
@@ -1,37 +1,38 @@
 import { ArrowForward } from '@mui/icons-material'
-import { Autocomplete, SelectChangeEvent, TextField } from '@mui/material'
+import { SelectChangeEvent } from '@mui/material'
 import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import React, { FC } from 'react'
 
+import CustomAutocomplete from './CustomAutocomplete'
 import Image from './assets/background.png'
 
 type Props = {
-  origin: string
-  destination: string
-  handleChangeOrigin: (event: SelectChangeEvent) => void
-  handleChangeDestination: (event: SelectChangeEvent) => void
+  sourceLanguage: string
+  targetLanguage: string
+  setSourceLanguage: (value: string) => void
+  setTargetLanguage: (value: string) => void
+  languages: string[]
 }
 
 const Title: FC<Props> = ({
-  origin,
-  destination,
-  handleChangeOrigin,
-  handleChangeDestination,
-}) => {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      sx={{
-        backgroundImage: `url(${Image})`,
-        backgroundSize: '100%',
-      }}
-    >
-      <Container maxWidth="sm" sx={{ marginTop: 15, marginBottom: 10 }}>
+  sourceLanguage,
+  targetLanguage,
+  setSourceLanguage,
+  setTargetLanguage,
+  languages,
+}) => (
+  <Box
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    sx={{
+      backgroundImage: `url(${Image})`,
+      backgroundSize: '100%',
+    }}
+  >
+    <Box maxWidth="sm">
+      <Box marginTop="20vh" marginBottom="15vh">
         <Typography variant="h1" align="center" gutterBottom>
           Essential expressions for traveling to any country
         </Typography>
@@ -39,39 +40,37 @@ const Title: FC<Props> = ({
           We grouped the most basic words you may need for a quick trip. In a
           clean and organized place, with audios included!
         </Typography>
-      </Container>
+      </Box>
 
-      <Stack
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        height="60px"
+        marginBottom="-30px"
+        zIndex="1"
+        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+        borderRadius="10px"
         sx={{
           backgroundColor: 'background.default',
-          borderRadius: '10px',
         }}
-        maxWidth="sm"
-        direction="row"
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
       >
-        <Autocomplete
-          disablePortal
-          id="source_language"
-          options={[{ label: 'English' }, { label: 'Français' }]}
-          renderInput={(params) => (
-            <TextField {...params} label="Source language" />
-          )}
+        <CustomAutocomplete
+          id="sourceLanguage"
+          value={sourceLanguage}
+          setValue={setSourceLanguage}
+          options={languages}
         />
         <ArrowForward />
-        <Autocomplete
-          disablePortal
-          id="target_language"
-          options={[{ label: 'English' }, { label: 'Français' }]}
-          renderInput={(params) => (
-            <TextField {...params} label="Target language" />
-          )}
+        <CustomAutocomplete
+          id="targetLanguage"
+          value={targetLanguage}
+          setValue={setTargetLanguage}
+          options={languages}
         />
-      </Stack>
+      </Box>
     </Box>
-  )
-}
+  </Box>
+)
 
 export default Title
