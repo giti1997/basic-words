afb8fe2b800ddb3082f3a3429ec6cba57c13da6f
Igor Silva <asilvaigor@gmail.com>
2022-10-20 23:48:08 +0200

    Start mobile autocomplete

--- front/src/CustomAutocomplete.tsx
+++ front/src/CustomAutocomplete.tsx
@@ -1,13 +1,20 @@
 import {
   Autocomplete,
-  Popper,
+  Button,
+  Dialog,
+  IconButton,
   TextField,
+  TextFieldProps,
+  Typography,
   autocompleteClasses,
   inputClasses,
   inputLabelClasses,
-  styled,
 } from '@mui/material'
+import { Box } from '@mui/system'
 import React, { FC, useState } from 'react'
+import { isMobile } from 'react-device-detect'
+
+import { ReactComponent as ArrowLeft } from './assets/arrowLeft.svg'
 
 type Props = {
   id: string
@@ -18,46 +25,93 @@ type Props = {
 
 const CustomAutocomplete: FC<Props> = ({ id, value, setValue, options }) => {
   const [inputValue, setInputValue] = useState(value)
+  const [open, setOpen] = React.useState(false)
 
-  return (
-    <Autocomplete
-      disablePortal
-      fullWidth
-      disableClearable
-      id={id}
-      forcePopupIcon={false}
-      renderInput={(params) => <TextField {...params} variant="standard" />}
-      options={options}
-      value={value}
-      inputValue={inputValue}
-      onChange={(_: any, newValue: string | null) => setValue(newValue ?? '')}
-      onInputChange={(_, newInputValue) => setInputValue(newInputValue)}
-      componentsProps={{
-        paper: {
-          sx: {
-            borderRadius: '10px',
-            boxShadow: 2,
-          },
-        },
-        popper: {
-          sx: {
-            [`& .${autocompleteClasses.option}`]: {
-              justifyContent: 'center',
+  const commonProps = {
+    id: id,
+    fullWidth: true,
+    value: value,
+    inputValue: inputValue,
+    onChange: (_: any, newValue: string | null) => {
+      if (newValue) {
+        setValue(newValue)
+        setOpen(false)
+      }
+    },
+    onInputChange: (_: any, newInputValue: string) =>
+      setInputValue(newInputValue),
+    options: options,
+    forcePopupIcon: false,
+    renderInput: (params: TextFieldProps) => (
+      <TextField {...params} variant="standard" />
+    ),
+  }
+
+  if (isMobile) {
+    return (
+      <>
+        <Button fullWidth sx={{ height: '100%' }} onClick={() => setOpen(true)}>
+          <Typography variant="body1" textTransform="none" fontWeight="regular">
+            {value}
+          </Typography>
+        </Button>
+        <Dialog fullScreen open={open} onClose={() => setOpen(false)}>
+          <Box display="flex">
+            <IconButton
+              color="inherit"
+              onClick={() => setOpen(false)}
+              aria-label="close"
+              sx={{ borderBottom: 1, borderRadius: 0 }}
+            >
+              <ArrowLeft />
+            </IconButton>
+            <Autocomplete
+              {...commonProps}
+              open={true}
+              componentsProps={{
+                paper: {
+                  sx: {
+                    borderRadius: '0px',
+                    boxShadow: 0,
+                  },
+                },
+              }}
+            />
+          </Box>
+        </Dialog>
+      </>
+    )
+  } else {
+    return (
+      <Autocomplete
+        disablePortal
+        {...commonProps}
+        disableClearable
+        componentsProps={{
+          paper: {
+            sx: {
+              borderRadius: '10px',
+              boxShadow: 2,
             },
           },
-        },
-      }}
-      sx={{
-        [`& .${inputLabelClasses.root}`]: { color: 'primary.light' },
-        [`& .${inputClasses.input}`]: { textAlign: 'center' },
-        [`& .${inputClasses.root}:before`]: {
-          borderBottom: 0,
-        },
-        // paddingLeft: '20px',
-        // paddingRight: '20px',
-      }}
-    />
-  )
+          popper: {
+            sx: {
+              [`& .${autocompleteClasses.option}`]: {
+                justifyContent: 'center',
+              },
+            },
+          },
+        }}
+        sx={{
+          [`& .${inputLabelClasses.root}`]: { color: 'primary.light' },
+          [`& .${inputClasses.input}`]: { textAlign: 'center' },
+          [`& .${inputClasses.root}:before`]: {
+            borderBottom: 0,
+          },
+        }}
+      />
+    )
+  }
 }
 
 export default CustomAutocomplete
