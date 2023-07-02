0932e27affef16591107e3204c86f84b943c580f
OMIDVAR Giti <giti.omidvar@inria.fr>
2023-07-02 15:07:19 +0200

    backend done

--- backend/api.py
+++ backend/api.py
@@ -2,16 +2,12 @@ from fastapi import FastAPI
 
 from google.cloud import bigquery
 
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../genuine-essence-364222-54dfa19feae0.json"
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../genuine-essence-364222-e5a8b39d8c83.json"
-
 app = FastAPI()
 
 # Construct a BigQuery client object.
 client = bigquery.Client()
 #set table_id
-table_id = "genuine-essence-364222.basic_words.words"
+table_id = "basic-words.basic_words.words"
 
 @app.get("/{language_id}")
 async def get_translations(language_id: str):
